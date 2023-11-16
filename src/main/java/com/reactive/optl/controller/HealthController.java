4a1701e2c87ca8a4b8390f53c51d01042e8d3fc7
LordMaduz <maduz.ruchira@gmail.com>
2023-11-16 18:37:40 +0800

    adding loki for monitoring logs.

--- src/main/java/com/reactive/optl/controller/HealthController.java
+++ src/main/java/com/reactive/optl/controller/HealthController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import java.time.Instant;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,8 +14,18 @@ import reactor.core.publisher.Mono;
 public class HealthController {
 
     @PostMapping("/health")
-    public Mono<ResponseEntity<String>> health(){
+    public Mono<ResponseEntity<String>> health() {
+        log.info("Request Received at : {}", Instant.now());
         return Mono.just(ResponseEntity.ok("Healthy"));
+    }
+
+
+    @PostMapping("/web-client-endpoint")
+    public Mono<ResponseEntity<String>> callEndpoint() {
+        return WebClient.builder().baseUrl("http://localhost:8080/health").build().post().retrieve().bodyToMono(String.class).map(response -> {
+            log.info("Response Received: {}", response);
+            return ResponseEntity.ok(response);
+        });
     }
 
 }
